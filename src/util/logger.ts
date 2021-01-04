4ce3c83d597b7c670d226d63046ea50d5bdffb1b
MSNTCS <najafi@codechain.io>
2021-01-04 18:45:36 +0900

    Add logger

---
+++ src/util/logger.ts
@@ -0,0 +1,30 @@
+import {
+    bgBlueBright,
+    bgYellow,
+    white,
+    bold,
+    bgRedBright,
+    redBright,
+    blueBright,
+    yellow,
+    greenBright,
+    bgGreenBright,
+} from 'chalk';
+
+export function info(text: string) {
+    console.log(bold(white(bgBlueBright(' INFO '))) + ' ' + blueBright(text));
+}
+
+export function warn(text: string) {
+    console.log(bold(white(bgYellow(' WARNING '))) + ' ' + yellow(text));
+}
+
+export function error(text: string) {
+    console.log(bold(white(bgRedBright(' ERROR '))) + ' ' + redBright(text));
+}
+
+export function success(text: string) {
+    console.log(
+        bold(white(bgGreenBright(' SUCCESS '))) + ' ' + greenBright(text)
+    );
+}
