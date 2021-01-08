f0da17f3c4aaa26e8549241af09511d9661fd608
MSNTCS <najafi@codechain.io>
2021-01-08 21:20:51 +0900

    Add interest messages

---
+++ src/commands/contracts/money-interest.ts
@@ -0,0 +1,44 @@
+import { CLIKey } from "@terra-money/terra.js/dist/key/CLIKey";
+import { AddressProviderFromEnvVar } from "../../anchor-js/address-provider";
+
+import { createExecMenu, handleExecCommand } from "../../util/contract-menu";
+import { Dec } from "@terra-money/terra.js";
+import { fabricatebInterestConfig } from "../../anchor-js/fabricators/money-market/interest-update-config";
+
+const mockAddressProvider = new AddressProviderFromEnvVar();
+const menu = createExecMenu(
+  "interest",
+  "Anchor MoneyMarket Interest contract functions"
+);
+
+interface Config {
+  owner?: string;
+  baseRate?: Dec;
+  interestMultiplier?: Dec;
+}
+
+const updateConfig = menu
+  .description("update config")
+  .option(
+    "--owner <AccAddress>",
+    "Address of contract owner that can update model parameters"
+  )
+  .option(
+    "--base-rate <Dec>",
+    "Minimum per-block interest rate applied to borrows"
+  )
+  .option(
+    "interes-multiplier <Dec>",
+    "Multiplier between utilization ratio and per-block borrow rate"
+  )
+  .action(async ({ owner, baseRate, interestMultiplier }: Config) => {
+    const key = new CLIKey({ keyName: updateConfig.from });
+    const userAddress = key.accAddress;
+    const msg = fabricatebInterestConfig({
+      address: userAddress,
+      owner: owner,
+      base_rate: baseRate,
+      interest_multiplier: interestMultiplier,
+    })(mockAddressProvider);
+    await handleExecCommand(menu, msg);
+  });
