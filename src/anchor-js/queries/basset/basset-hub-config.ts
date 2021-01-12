6829ae0865c98ec66528d19d2a0d9dfc0045d156
MSNTCS <najafi@codechain.io>
2021-01-12 15:37:19 +0900

    Add response message for config query of hub

--- src/anchor-js/queries/basset/basset-hub-config.ts
+++ src/anchor-js/queries/basset/basset-hub-config.ts
@@ -5,12 +5,18 @@ interface Option {
   lcd: LCDClient;
   bAsset: string;
 }
+interface ConfigResponse{
+    owner: string,
+    reward_contract?: string,
+    token_contract?: string
+}
 
-export const queryHubConfig = ({ lcd, bAsset }: Option) => (
-  addressProvider: AddressProvider.Provider
-): any => {
-  const bAssetContractAddress = addressProvider.bAssetHub(bAsset);
-  lcd.wasm.contractQuery(bAssetContractAddress, {
-    config: {},
-  });
+export const queryHubConfig = ({ lcd, bAsset }: Option) =>  async (
+    addressProvider: AddressProvider.Provider
+): Promise<ConfigResponse> => {
+    const bAssetContractAddress = addressProvider.bAssetHub(bAsset);
+    let reponse: ConfigResponse = await lcd.wasm.contractQuery(bAssetContractAddress, {
+        config: {},
+    });
+    return reponse;
 };
