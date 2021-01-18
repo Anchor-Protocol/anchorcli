4cd6957768f9371ec71e4107e812aa9aef544950
r-amiri <65209767+r-amiri@users.noreply.github.com>
2021-01-18 12:51:34 +0900

    Money market Overseer queries

---
+++ src/anchor-js/queries/money-market/overseer-borrow-limit.ts
@@ -0,0 +1,35 @@
+import { LCDClient } from "@terra-money/terra.js";
+import { AddressProvider } from "../../address-provider/types";
+import { stringifyNumber } from "yaml/util";
+
+interface Option {
+  lcd: LCDClient;
+  overseer: string;
+  borrower: string;
+  blockTime?: number;
+}
+interface BorrowLimitResponse {
+  borrower: string;
+  borrowLimit: string;
+}
+
+export const queryOverseerBorrowLimit = ({
+  lcd,
+  overseer,
+  borrower,
+  blockTime,
+}: Option) => async (
+  addressProvider: AddressProvider.Provider
+): Promise<BorrowLimitResponse> => {
+  const overseerContractAddress = addressProvider.overseer(overseer);
+  let response: BorrowLimitResponse = await lcd.wasm.contractQuery(
+    overseerContractAddress,
+    {
+      borrowLimit: {
+        borrower: borrower,
+        blockTime: blockTime,
+      },
+    }
+  );
+  return response;
+};
---
+++ src/anchor-js/queries/money-market/overseer-epoch-state.ts
@@ -0,0 +1,26 @@
+import { LCDClient } from "@terra-money/terra.js";
+import { AddressProvider } from "../../address-provider/types";
+
+interface Option {
+  lcd: LCDClient;
+  overseer: string;
+}
+interface EpochStateResponse {
+  depositRate: string;
+  prevATokenSupply: string;
+  prevExchangeRate: string;
+  lastExecutedHeight: number;
+}
+
+export const queryOverseerEpochState = ({ lcd, overseer }: Option) => async (
+  addressProvider: AddressProvider.Provider
+): Promise<EpochStateResponse> => {
+  const overseerContractAddress = addressProvider.overseer(overseer);
+  let response: EpochStateResponse = await lcd.wasm.contractQuery(
+    overseerContractAddress,
+    {
+      epochState: {},
+    }
+  );
+  return response;
+};
