023d7890d0eb7fcfea9eae0e999bfd1703cb9dc1
r-amiri <65209767+r-amiri@users.noreply.github.com>
2021-01-18 14:19:40 +0900

    Money market Oracle queries

---
+++ src/anchor-js/queries/money-market/oracle-prices.ts
@@ -0,0 +1,27 @@
+import { LCDClient } from "@terra-money/terra.js";
+import { AddressProvider } from "../../address-provider/types";
+
+interface Option {
+  lcd: LCDClient;
+  startAfter?: string;
+  limit?: number;
+}
+interface PricesResponse {
+  prices: object[];
+}
+
+export const queryOraclePrice = ({ lcd, startAfter, limit }: Option) => async (
+  addressProvider: AddressProvider.Provider
+): Promise<PricesResponse> => {
+  const oracleContractAddress = addressProvider.oracle();
+  let response: PricesResponse = await lcd.wasm.contractQuery(
+    oracleContractAddress,
+    {
+      prices: {
+        startAfter: startAfter,
+        limit: limit,
+      },
+    }
+  );
+  return response;
+};
