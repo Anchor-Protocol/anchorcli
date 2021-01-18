485ddad6c8b3382419bfd4b8e0b0de1b42a4d531
r-amiri <65209767+r-amiri@users.noreply.github.com>
2021-01-18 14:19:40 +0900

    Money market Oracle queries

---
+++ src/anchor-js/queries/money-market/oracle-config.ts
@@ -0,0 +1,23 @@
+import { LCDClient } from "@terra-money/terra.js";
+import { AddressProvider } from "../../address-provider/types";
+
+interface Option {
+  lcd: LCDClient;
+}
+interface ConfigResponse {
+  owner: string;
+  baseAsset: string;
+}
+
+export const queryOracleConfig = ({ lcd }: Option) => async (
+  addressProvider: AddressProvider.Provider
+): Promise<ConfigResponse> => {
+  const oracleContractAddress = addressProvider.oracle();
+  let response: ConfigResponse = await lcd.wasm.contractQuery(
+    oracleContractAddress,
+    {
+      config: {},
+    }
+  );
+  return response;
+};
